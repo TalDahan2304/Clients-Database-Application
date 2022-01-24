78c80b5e031a435587ea781b3d3d94560bec5b37
TalDahan2304 <tald234097@gmail.com>
2022-01-25 00:55:12 +0200

    server database

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser= require('body-parser');
+const cors= require('cors');
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "mysqlpassword",
+    database: "humanzdb",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/get', (req, res)=>{
+    const sqlSelect=
+    "SELECT * FROM clients_db";
+    db.query(sqlSelect, (err, result)=>{
+        res.send(result);
+
+ });
+})
+ //989
+
+app.post("/api/insert", (req, res)=>{
+    const fullName=req.body.fullName;
+    const ID=req.body.ID;
+    const ipAddress=req.body.ipAddress;
+    const phoneNumber=req.body.phoneNumber; 
+
+    const sqlInsert=
+    "INSERT INTO clients_db (fullName, ID, ipAddress, phoneNumber) VALUES ( ?, ?, ?, ?)";
+    db.query(sqlInsert, [fullName,ID,ipAddress,phoneNumber], (err, result)=>{
+        console.log(result);
+
+    });
+});
+
+app.listen(3003, ()=> {
+    console.log("running on port 3003");
+});
